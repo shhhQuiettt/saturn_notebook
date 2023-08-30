29b5358d2dec91d12e00430ba89c1e7aea7e01ff
shhhQuiettt <krzysztof.skro@gmail.com>
2023-08-30 12:42:32 +0200

    Set up client and shell subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,69 @@
-use jupyter_client::Client;
-use std::time;
-use std::fs::File;
-use std::thread;
-mod cell;
+// extern crate serde;
+// extern crate serde_json;
 
-const KERNEL_NO: &str = "922269";
+use serde::Deserialize;
+use serde_json;
+use std::fs::File;
+use zmq;
+
+//TODO: get it automatically
+const KERNEL_NO: &str = "277756";
+
+#[derive(Deserialize, Debug)]
+struct KernelData {
+    shell_port: u16,
+    iopub_port: u16,
+    stdin_port: u16,
+    control_port: u16,
+    hb_port: u16,
+    ip: String,
+    key: String,
+    transport: String,
+    signature_scheme: String,
+    kernel_name: String,
+}
+
+impl KernelData {
+    fn new() -> KernelData {
+        let file = get_kernel_file();
+        let kernel_data: KernelData = serde_json::from_reader(file).unwrap();
+        return kernel_data;
+    }
+}
+
+struct ShellSubscriber {
+    // zmq_context: zmq::Context,
+    zmq_socket: zmq::Socket,
+}
+
+struct Client {
+    kernel_data: KernelData,
+    zmq_context: zmq::Context,
+    // zmq_socket: zmq::Socket,
+}
+
+impl Client {
+    fn new(kernel_data: KernelData) -> Client {
+        let zmq_context = zmq::Context::new();
+        return Client {
+            kernel_data,
+            zmq_context,
+        };
+    }
+
+    fn subscribe_to_shell(&self) -> ShellSubscriber {
+        let zmq_socket = self.zmq_context.socket(zmq::SUB).expect("Failed to create shell socket");
+        let shell_port = self.kernel_data.shell_port;
+        let ip = &self.kernel_data.ip;
+        let address = format!("tcp://{}:{}", ip, shell_port);
+        zmq_socket.connect(&address).expect("Cannot connect to the kernel shell");
+        zmq_socket.set_subscribe(b"").expect("Cannot subscribe to the kernel shell");
+        return ShellSubscriber {
+            // zmq_context: self.zmq_context,
+            zmq_socket,
+        };
+    }
+}
 
 fn get_kernel_file() -> File {
     let path = "/home/krzys/.local/share/jupyter/runtime/kernel-".to_owned() + KERNEL_NO + ".json";
@@ -12,51 +71,8 @@ fn get_kernel_file() -> File {
 }
 
 fn main() {
-    let file = get_kernel_file();
-    let client = Client::from_reader(file).unwrap();
-
-    let io_receiver = client.iopub_subscribe().unwrap();
-    thread::spawn(move || {
-        for msg in io_receiver {
-            println!("Received message from kernel: {:#?}", msg);
-
-            // println!("Received message from kernel");
-        }
-    });
-
-    let c = cell::Cell::new("410 + 10".to_owned());
-    c.execute(&client);
-
-    // Set up the heartbeat watcher
-    // let hb_receiver = client.heartbeat();
-    // thread::spawn(move || {
-    //     for _ in hb_receiver {
-    //         println!("Received heartbeat from kernel");
-    //     }
-    // });
-    // let command = Command::Execute {
-    //     code: "3+8".to_string(),
-    //     silent: false,
-    //     store_history: true,
-    //     user_expressions: HashMap::new(),
-    //     allow_stdin: true,
-    //     stop_on_error: false,
-    // };
-
-    // // Run some code on the kernel
-    // let response = client.send_shell_command(command);
-    // match response {
-    //     Ok(_r) => {
-    //         // println!("{:?}", r);
-    //         // match r {
-    //         //     Response::Shell(m) => println!("{:?}", m),
-    //         //     Response::IoPub(m) => println!("{:?}", m),
-    //         // }
-    //     }
-    //     Err(e) => {
-    //         println!("{}", e)
-    //     }
-    // }
-    thread::sleep(time::Duration::new(1, 0));
-    // }
+    let kernel_data = KernelData::new();
+    println!("{:#?}", kernel_data);
+    let client = Client::new(kernel_data);
+    let _shell_subscriber = client.subscribe_to_shell();
 }
